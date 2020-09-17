259ba8e4a0c11230f9330cca9bcf3c4b4d332c19
devdammit <ant.shchurov@gmail.com>
2020-09-17 16:34:28 +0300

    Added task - firstDuplicate

---
+++ src/first_duplicate.spec.ts
@@ -0,0 +1,25 @@
+import { firstDuplicate } from './first_duplicate'
+
+describe('First duplicate', () => {
+    it('should be return first duplicate number for which the second occurrence has the minimal index', () => {
+        firstDuplicate([2, 1, 3, 5, 3, 2]).tap({
+            right: value => expect(value).toBe(3),
+        })
+
+        firstDuplicate([2, 2]).tap({
+            right: value => expect(value).toBe(2),
+        })
+    })
+
+    it('should be return -1 when no such elements', () => {
+        const expectedCb = value => expect(value).toBe(-1)
+
+        firstDuplicate([2, 4, 3, 5, 1]).tap({
+            right: expectedCb,
+        })
+
+        firstDuplicate([]).tap({
+            right: expectedCb,
+        })
+    })
+})
